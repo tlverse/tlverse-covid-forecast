6ef93eb78cbbdf3e74d0dc11a5354642caf34f14
Jeremy Coyle <jeremyrcoyle@gmail.com>
2020-03-24 16:33:27 -0700

    small fixes

--- tlversecovidforecast/R/setup.R
+++ tlversecovidforecast/R/setup.R
@@ -19,6 +19,7 @@ generate_analytic_data <- function(){
   data[, case10_days:=as.numeric(difftime(date, tenth_case_date,unit="days")), by=list(region)]
   data[, hundreth_case_date:=min(date[cases>100]), by=list(region)]
   data[, case100_days:=as.numeric(difftime(date, hundreth_case_date,unit="days")), by=list(region)]
+  data[, max_cases:=max(cases), by=list(region)]
   
   return(data)
 }
@@ -27,24 +28,35 @@ generate_task <- function(){
   covariates <- c("case_days","log_case_days")
   
   #TODO: carefully consider fold structure
+  folds <- make_folds(n=NULL, t=NULL, id=NULL, time=NULL, 
+                      first_window = NULL, validation_size=30,
+                      gap = 0, batch=1, 
+                      timefold_fun = folds_rolling_origin_pooled)
+  # (time series structures, but also consider multiple regions)
   task <- make_sl3_Task(main_data, outcome="cases", covariates = covariates, id="region")
   return(task)
 }
 
 
 loss_competition <- function(pred, observed){
-  # TODO: put bound in learner, not loss
-  pred <- pmax(pred, 0)
+
   loss <- (log(pred+1)-log(observed + 1))^2
+
   return(loss)
+}
+
+metalearner_linear_bound <- function(alpha, X){
+  return( pmax(metalearner_linear(alpha,X),0))
 }
 
 #' @import sl3
 generate_learners <- function(){
+  #TODO integrate timeseries learners + mechanistic models
+  
   lrnr_glm <- make_learner(Lrnr_glm)
   lrnr_rf <- make_learner(Lrnr_randomForest)
   
-  metalearner_competition <- make_learner(Lrnr_solnp, metalearner_linear, loss_competition)
+  metalearner_competition <- make_learner(Lrnr_solnp, metalearner_linear_bound, loss_competition)
   
   sl <- make_learner(Lrnr_sl, list(lrnr_glm, lrnr_rf), metalearner_competition)
   
