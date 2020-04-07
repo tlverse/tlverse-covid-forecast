9c3934d856e322c6d0726633304c6308cecc2834
rachaelvphillips <rachaelvphillips@gmail.com>
2020-04-07 15:58:51 -0600

    update covs and add args for playing

--- tlversecovidforecast/R/sl3_setup.R
+++ tlversecovidforecast/R/sl3_setup.R
@@ -3,71 +3,99 @@
 #' @import origami
 
 covariate_list <- function() {
-  # TODO: fix continent and add back in
-  c(
-    "delta_rail_millionpassengerkm",
-    "days", "case_days",
-    "case10_days", "case100_days", "recoveries",
-    "days_quarantine", "quarantine", "days_restrictions", "restrictions",
-    "days_schools_national", "schools_national", "days_schools_localized",
-    "schools_localized", "lat", "lon", "population", "area", "sars_cases",
-    "sars_deaths", "sars_recovered", "delta_recoveries",
-    "tests", "density", "median_age", "urbanpop", "hospital_bed",
-    "smokers", "sex0", "sex14", "sex25", "sex54", "sex64", "sex65plus",
-    "sex_ratio", "lung_disease", "femalelung", "malelung", "obese",
-    "male_obese", "fem_obese", "overweight", "male_overweight", "fem_overweight",
-    "air_passengers", "econ_worldrank", "econ_regionrank",
-    "econ_2019score", "econ_propertyrights", "econ_judical", "econ_gov_integrity",
-    "econ_taxburden", "econ_govspending", "econ_fiscalhealth", "econ_businessfreedom",
-    "econ_laborfreedom", "econ_monetaryfreedom", "econ_tradefreedom",
-    "econ_investmentfreedom", "econ_financialfreedom", "econ_tariffrate",
-    "econ_incometaxrate", "econ_corporatetaxrate", "econ_taxburdengdp",
-    "econ_govexpendgdp", "econ_popmills", "econ_gdpbills", "econ_gdpgrowthrate",
-    "econ_yeargdpgrowthrate", "econ_gdppercapita", "econ_unemployment",
-    "econ_inflation", "econ_fdiflowmills", "econ_publicdeptofgdp",
-    "gdp_2018", "crime_index", "population2020", "smoking2016", "females2018",
-    "hospbeds_per1k", "docs_per1k",
-    "pollution_2017",
-    "pop65above_percent",
-    "prison_count", "prison_rate",
-    "rail_millionpassengerkm", "delta_tests",
-    "delta_density", "delta_median_age", "delta_urbanpop", "delta_hospital_bed",
-    "delta_smokers", "delta_sex0", "delta_sex14", "delta_sex25",
-    "delta_sex54", "delta_sex64", "delta_sex65plus", "delta_sex_ratio",
-    "delta_lung_disease", "delta_femalelung", "delta_malelung", "delta_obese",
-    "delta_male_obese", "delta_fem_obese", "delta_overweight", "delta_male_overweight",
-    "delta_fem_overweight", "delta_air_year", "delta_air_passengers",
-    "delta_econ_worldrank", "delta_econ_regionrank", "delta_econ_2019score",
-    "delta_econ_propertyrights", "delta_econ_judical", "delta_econ_gov_integrity",
-    "delta_econ_taxburden", "delta_econ_govspending", "delta_econ_fiscalhealth",
-    "delta_econ_businessfreedom", "delta_econ_laborfreedom", "delta_econ_monetaryfreedom",
-    "delta_econ_tradefreedom", "delta_econ_investmentfreedom", "delta_econ_financialfreedom",
-    "delta_econ_tariffrate", "delta_econ_incometaxrate", "delta_econ_corporatetaxrate",
-    "delta_econ_taxburdengdp", "delta_econ_govexpendgdp", "delta_econ_popmills",
-    "delta_econ_gdpbills", "delta_econ_gdpgrowthrate", "delta_econ_yeargdpgrowthrate",
-    "delta_econ_gdppercapita", "delta_econ_unemployment", "delta_econ_inflation",
-    "delta_econ_fdiflowmills", "delta_econ_publicdeptofgdp", "delta_gdp_2018",
-    "delta_crime_index", "delta_population2020", "delta_smoking2016",
-    "delta_females2018", "delta_hospbeds_year", "delta_hospbeds_per1k",
-    "delta_docs_year", "delta_docs_per1k", "delta_pollution_2010",
-    "delta_pollution_2011", "delta_pollution_2012", "delta_pollution_2013",
-    "delta_pollution_2014", "delta_pollution_2015", "delta_pollution_2016",
-    "delta_pollution_2017", "delta_pop65above_year", "delta_pop65above_percent",
-    "delta_prisoncount_year", "delta_prison_count", "delta_prisonrate_year",
-    "delta_prison_rate", "delta_rail_year"
-  )
+  c("recoveries", "days_quarantine", "quarantine", "days_restrictions", 
+    "restrictions", "days_schools_national", "schools_national", 
+    "days_schools_localized", "schools_localized", "lat", "lon", "continent", 
+    "population", "area", "sars_cases", "sars_deaths", "sars_recovered", 
+    "delta_recoveries", "continent", "subregion", "tests", "density", 
+    "median_age", "urbanpop", "hospital_bed", "smokers", "sex0", "sex14", 
+    "sex25", "sex54", "sex64", "sex65plus", "sex_ratio", "lung_disease", 
+    "femalelung", "malelung", "obese", "male_obese", "fem_obese", "overweight", 
+    "male_overweight", "fem_overweight", "air_year", "air_passengers", 
+    "econ_worldrank", "econ_regionrank", "econ_2019score", 
+    "econ_propertyrights", "econ_judical", "econ_gov_integrity", 
+    "econ_taxburden", "econ_govspending", "econ_fiscalhealth", 
+    "econ_businessfreedom", "econ_laborfreedom", "econ_monetaryfreedom", 
+    "econ_tradefreedom", "econ_investmentfreedom", "econ_financialfreedom", 
+    "econ_tariffrate", "econ_incometaxrate", "econ_corporatetaxrate", 
+    "econ_taxburdengdp", "econ_govexpendgdp", "econ_popmills", "econ_gdpbills", 
+    "econ_gdpgrowthrate", "econ_yeargdpgrowthrate", "econ_gdppercapita", 
+    "econ_unemployment", "econ_inflation", "econ_fdiflowmills", 
+    "econ_publicdeptofgdp", "gdp_2018", "crime_index", "population2020", 
+    "smoking2016", "females2018", "hospbeds_year", "hospbeds_per1k", 
+    "docs_year", "docs_per1k", "pollution_2010", "pollution_2011", 
+    "pollution_2012", "pollution_2013", "pollution_2014", "pollution_2015", 
+    "pollution_2016", "pollution_2017", "pop65above_year", "pop65above_percent", 
+    "prisoncount_year", "prison_count", "prisonrate_year", "prison_rate", 
+    "rail_year", "rail_millionpassengerkm", "delta_tests", "delta_density", 
+    "delta_median_age", "delta_urbanpop", "delta_hospital_bed", "delta_smokers", 
+    "delta_sex0", "delta_sex14", "delta_sex25", "delta_sex54", "delta_sex64", 
+    "delta_sex65plus", "delta_sex_ratio", "delta_lung_disease", 
+    "delta_femalelung", "delta_malelung", "delta_obese", "delta_male_obese", 
+    "delta_fem_obese", "delta_overweight", "delta_male_overweight", 
+    "delta_fem_overweight", "delta_air_year", "delta_air_passengers", 
+    "delta_econ_worldrank", "delta_econ_regionrank", "delta_econ_2019score", 
+    "delta_econ_propertyrights", "delta_econ_judical", 
+    "delta_econ_gov_integrity", "delta_econ_taxburden", 
+    "delta_econ_govspending", "delta_econ_fiscalhealth", 
+    "delta_econ_businessfreedom", "delta_econ_laborfreedom", 
+    "delta_econ_monetaryfreedom", "delta_econ_tradefreedom", 
+    "delta_econ_investmentfreedom", "delta_econ_financialfreedom", 
+    "delta_econ_tariffrate", "delta_econ_incometaxrate", 
+    "delta_econ_corporatetaxrate", "delta_econ_taxburdengdp", 
+    "delta_econ_govexpendgdp", "delta_econ_popmills", "delta_econ_gdpbills", 
+    "delta_econ_gdpgrowthrate", "delta_econ_yeargdpgrowthrate", 
+    "delta_econ_gdppercapita", "delta_econ_unemployment", 
+    "delta_econ_inflation", "delta_econ_fdiflowmills", 
+    "delta_econ_publicdeptofgdp", "delta_gdp_2018", "delta_crime_index", 
+    "delta_population2020", "delta_smoking2016", "delta_females2018", 
+    "delta_hospbeds_year", "delta_hospbeds_per1k", "delta_docs_year", 
+    "delta_docs_per1k", "delta_pollution_2010", "delta_pollution_2011", 
+    "delta_pollution_2012", "delta_pollution_2013", "delta_pollution_2014", 
+    "delta_pollution_2015", "delta_pollution_2016", "delta_pollution_2017", 
+    "delta_pop65above_year", "delta_pop65above_percent", 
+    "delta_prisoncount_year", "delta_prison_count", "delta_prisonrate_year", 
+    "delta_prison_rate", "delta_rail_year", "delta_rail_millionpassengerkm",
+    "days", "first_case_date", "tenth_case_date", "hundreth_case_date", 
+    "case_days", "case10_days", "case100_days", "max_cases", "log_recoveries", 
+    "log_population", "log_area", "log_sars_cases", "log_sars_deaths", 
+    "log_sars_recovered", "log_tests", "log_density", "log_median_age", 
+    "log_urbanpop", "log_hospital_bed", "log_smokers", "log_sex0", "log_sex14", 
+    "log_sex25", "log_sex54", "log_sex64", "log_sex65plus", "log_sex_ratio", 
+    "log_lung_disease", "log_femalelung", "log_malelung", "log_obese", 
+    "log_male_obese", "log_fem_obese", "log_overweight", "log_male_overweight", 
+    "log_fem_overweight", "log_air_passengers", "log_econ_2019score", 
+    "log_econ_propertyrights", "log_econ_judical", "log_econ_gov_integrity",
+    "log_econ_taxburden", "log_econ_govspending", "log_econ_fiscalhealth", 
+    "log_econ_businessfreedom", "log_econ_laborfreedom", 
+    "log_econ_monetaryfreedom", "log_econ_tradefreedom", 
+    "log_econ_investmentfreedom", "log_econ_financialfreedom", 
+    "log_econ_tariffrate", "log_econ_incometaxrate", 
+    "log_econ_corporatetaxrate", "log_econ_taxburdengdp", 
+    "log_econ_govexpendgdp", "log_econ_popmills", "log_econ_gdpbills", 
+    "log_econ_gdppercapita", "log_econ_unemployment", 
+    "log_econ_publicdeptofgdp", "log_gdp_2018", "log_crime_index", 
+    "log_population2020", "log_smoking2016", "log_females2018", 
+    "log_hospbeds_per1k", "log_docs_per1k", "log_pollution_2010", 
+    "log_pollution_2011", "log_pollution_2012", "log_pollution_2013",
+    "log_pollution_2014", "log_pollution_2015", "log_pollution_2016", 
+    "log_pollution_2017", "log_pop65above_year", "log_pop65above_percent", 
+    "log_prison_count", "log_prison_rate", "log_rail_millionpassengerkm", 
+    "log_max_cases", "weekday")
 }
-generate_task <- function(data, outcome) {
 
-  # covariates <- colnames(data)[-which(names(data) %in% c("cases","region"))]
+generate_task <- function(data, outcome, first_window = 20, time = data$days,
+                          t = max(data$days), batch = 1) {
+
   folds <- origami::make_folds(data,
-    t = max(data$days), id = data$region,
-    time = data$days,
+    t = t, 
+    id = data$region,
+    time = time,
     fold_fun = folds_rolling_origin_pooled,
-    first_window = 20,
+    first_window = first_window,
     validation_size = 30,
     gap = 0,
-    batch = 1
+    batch = batch
   )
 
   # TODO: consider imputation of covariates, drop_missing_outcome
@@ -95,40 +123,50 @@ metalearner_linear_bound <- function(alpha, X) {
 }
 
 #' @import sl3
-generate_learners <- function(variable_stratify = "continent") {
-  # TODO integrate timeseries learners + mechanistic models
-  grid_params <- list(
-    max_depth = c(2, 4, 6, 8),
-    eta = c(0.001, 0.01, 0.1, 0.2, 0.3)
-  )
-  grid <- expand.grid(grid_params, KEEP.OUT.ATTRS = FALSE)
-  params_default <- list(nthread = getOption("sl.cores.learners", 1))
-  xgb_learners <- apply(grid, MARGIN = 1, function(params_tune) {
-    do.call(Lrnr_xgboost$new, c(params_default, as.list(params_tune)))
-  })
-  lrnr_lasso <- make_learner(Lrnr_glmnet, alpha = 1)
-  lrnr_glm <- make_learner(Lrnr_glm)
-  lrnr_ranger <- make_learner(Lrnr_ranger)
-  lrnr_earth <- make_learner(Lrnr_earth)
-  lrnr_gts <- make_learner(Lrnr_gts)
-  stack <- make_learner(Stack, unlist(list(
-    xgb_learners, lrnr_glm, lrnr_lasso,
-    lrnr_ranger, lrnr_earth, lrnr_gts
-  ),
-  recursive = TRUE
-  ))
-  lrnr_glmnet <- make_learner(Lrnr_glmnet)
-  screener <- make_learner(Lrnr_screener_coefs, lrnr_glmnet, threshold = 1e-2)
-  screener_flex <- make_learner(Lrnr_screener_coefs, lrnr_glmnet, threshold = 1e-3)
-  pipe <- make_learner(Pipeline, screener, stack)
-  pipe_flex <- make_learner(Pipeline, screener_flex, stack)
-  multiscreen_stack <- make_learner(Stack, pipe, pipe_flex)
+generate_learners <- function(variable_stratify = "continent", stack = NULL) {
+  
+  if(is.null(stack)){
+    # TODO integrate timeseries learners + mechanistic models
+    
+    # base library
+    grid_params <- list(
+      max_depth = c(2, 4, 6, 8),
+      eta = c(0.001, 0.01, 0.1, 0.2, 0.3)
+    )
+    grid <- expand.grid(grid_params, KEEP.OUT.ATTRS = FALSE)
+    params_default <- list(nthread = getOption("sl.cores.learners", 1))
+    xgb_learners <- apply(grid, MARGIN = 1, function(params_tune) {
+      do.call(Lrnr_xgboost$new, c(params_default, as.list(params_tune)))
+    })
+    lrnr_lasso <- make_learner(Lrnr_glmnet, alpha = 1)
+    lrnr_glm <- make_learner(Lrnr_glm)
+    lrnr_ranger <- make_learner(Lrnr_ranger)
+    lrnr_earth <- make_learner(Lrnr_earth)
+    lrnr_gts <- make_learner(Lrnr_gts)
+    stack <- make_learner(Stack, unlist(list(xgb_learners, lrnr_glm, lrnr_lasso,
+                                             lrnr_ranger, lrnr_earth, lrnr_gts), 
+                                        recursive = TRUE))
+    
+    # screeners
+    lrnr_glmnet <- make_learner(Lrnr_glmnet)
+    screener <- make_learner(Lrnr_screener_coefs, lrnr_glmnet, threshold = 1e-2)
+    screener_flex <- make_learner(Lrnr_screener_coefs, lrnr_glmnet, threshold = 1e-3)
+    pipe <- make_learner(Pipeline, screener, stack)
+    pipe_flex <- make_learner(Pipeline, screener_flex, stack)
+    
+    # stack
+    stack <- make_learner(Stack, pipe, pipe_flex)
+  }
+  
+  # metalearner
   metalearner_competition <- make_learner(
     Lrnr_solnp, metalearner_linear_bound,
     loss_squared_error
   )
   stratified_metalearner <- Lrnr_stratified$new(learner = metalearner_competition,
                                                 variable_stratify = variable_stratify)
-  sl <- make_learner(Lrnr_sl, multiscreen_stack, stratified_metalearner)
+  
+  # super learner
+  sl <- make_learner(Lrnr_sl, stack, stratified_metalearner)
   return(sl)
 }
