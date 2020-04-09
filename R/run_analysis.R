364227f60e0abc7b6624f78cca3fd08e317baf4d
Nima Hejazi <nh@nimahejazi.org>
2020-04-09 12:52:53 -0700

    tweaks

--- R/run_analysis.R
+++ R/run_analysis.R
@@ -16,37 +16,34 @@ plan(multiprocess, workers = future_cores)
 devtools::document(here("tlversecovidforecast"))
 devtools::load_all(here("tlversecovidforecast"))
 
-# read data, make learners 
+# make SL learners and read in data
 sl3_debug_mode()
 sl <- generate_learners()
 data <- read_csv(here("Data", "training_processed.csv"))
 test_data <- read_csv(here("Data", "test_processed.csv"))
 
-# generate case preds
+# generate case task and training predictions
 log_cases_task <- generate_task(data, "log_cases")
-test_log_cases_task <- generate_task(test_data, "log_cases")
-
 cases_fit <- sl$train(log_cases_task)
 cv_risk_table <- cases_fit$cv_risk(loss_squared_error)
 print(cv_risk_table)
 
+# for testing data, generate case task and predictions
+test_log_cases_task <- generate_task(test_data, "log_cases")
 test_preds <- cases_fit$predict(test_log_cases_task)
 test_cases_preds <- exp(test_preds) - 1
 
-# generate fatalities preds
+# generate fatalities predictions
 log_fatalities_task <- generate_task(data, "log_fatalities")
-test_log_fatalities_task <- generate_task(test_data, "log_fatalities")
-
 fatalities_fit <- sl$train(log_fatalities_task)
+test_log_fatalities_task <- generate_task(test_data, "log_fatalities")
 test_preds <- fatalities_fit$predict(test_log_fatalities_task)
 test_fatalities_preds <- exp(test_preds) - 1
 
 # generate submission
 ex_submission <- fread(here("Data", "week2", "submission.csv"))
-names(ex_submission)
-
-submission <- data.table(ForecastId = test_data$forecastid,
-                         ConfirmedCases = test_cases_preds,
-                         Fatalities = test_fatalities_preds)
+submission <- as.data.table(list(test_data$forecastid, test_cases_preds,
+                                 test_fatalities_preds))
+setnames(submission, names(ex_submission))
 submission <- submission[order(ForecastId)]
 write_csv(submission, here("Data", "tlverse_submission.csv"))
