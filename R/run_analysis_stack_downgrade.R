c06b9eada2d287b0a03af6e83855a2b67d3a850d
Nima Hejazi <nh@nimahejazi.org>
2020-04-11 14:22:27 -0700

    changes for fast run

--- R/run_analysis_stack_downgrade.R
+++ R/run_analysis_stack_downgrade.R
@@ -1,62 +1,88 @@
-start <- proc.time()
 library(here)
 library(tidyverse)
 library(data.table)
-devtools::install_github("tlverse/sl3@timeseries-overhaul")
+remotes::install_github("tlverse/origami@devel")
+remotes::install_github("tlverse/sl3@timeseries-overhaul")
+library(future)
 library(sl3)
 
+# get/set miniconda python for kerasR
+reticulate::use_python(system("which python3"))
+
+# data.table threading vs. future (probably unnecessary)
+dt_cores <- as.integer(unname(round(0.2 * availableCores())))
+future_cores <- as.integer(unname(availableCores()) - dt_cores)
+setDTthreads(dt_cores)
+plan(multiprocess, workers = future_cores)
+
 # load helper package
-devtools::document("tlversecovidforecast")
-devtools::load_all("tlversecovidforecast")
+devtools::document(here("tlversecovidforecast"))
+devtools::load_all(here("tlversecovidforecast"))
 
 ### base library 
 grid_params <- list(
-  max_depth = c(2, 4, 6, 8),
-  eta = c(0.001, 0.01, 0.1, 0.2, 0.3)
-)
-grid <- expand.grid(grid_params, KEEP.OUT.ATTRS = FALSE)
+  max_depth = c(2, 5, 8),
+  eta = c(0.001, 0.05, 0.2),
+  nrounds = c(50, 100)
+) %>% expand.grid(KEEP.OUT.ATTRS = FALSE)
 params_default <- list(nthread = getOption("sl.cores.learners", 1))
-xgb_learners <- apply(grid, MARGIN = 1, function(params_tune) {
+xgb_lrnrs <- apply(grid_params, MARGIN = 1, function(params_tune) {
   do.call(Lrnr_xgboost$new, c(params_default, as.list(params_tune)))
 })
-lrnr_lasso <- make_learner(Lrnr_glmnet)
-lrnr_glm <- make_learner(Lrnr_glm)
-lrnr_ranger <- make_learner(Lrnr_ranger)
-lrnr_earth <- make_learner(Lrnr_earth)
-lrnr_polspline <- make_learner(Lrnr_polspline)
+mean_lrnr <- Lrnr_mean$new()
+glm_lrnr <- Lrnr_glm$new()
+ridge_lrnr <- Lrnr_glmnet$new(alpha = 0, nfolds = 3)
+lasso_lrnr <- Lrnr_glmnet$new(alpha = 1, nfolds = 3)
+enet_lrnr_reg25 <- Lrnr_glmnet$new(alpha = 0.25, nfolds = 3)
+enet_lrnr_reg50 <- Lrnr_glmnet$new(alpha = 0.50, nfolds = 3)
+enet_lrnr_reg75 <- Lrnr_glmnet$new(alpha = 0.75, nfolds = 3)
+ranger_lrnr_base <- Lrnr_ranger$new()
+ranger_lrnr_nt50 <- Lrnr_ranger$new(num.trees = 50)
+ranger_lrnr_nt100 <- Lrnr_ranger$new(num.trees = 100)
+ranger_lrnr_nt500 <- Lrnr_ranger$new(num.trees = 500)
+earth_lrnr <- make_learner(Lrnr_earth)
 
 # time series learners
-lrnr_alan <- make_learner(Lrnr_alan_pois)
-lrnr_gts <- make_learner(Lrnr_gts)
-lrnr_arima <- make_learner(Lrnr_arima)
-lrnr_expSmooth <- make_learner(Lrnr_expSmooth)
+alan_lrnr <- make_learner(Lrnr_alan_pois)
+gts_lrnr <- make_learner(Lrnr_gts)
+arima_lrnr <- make_learner(Lrnr_arima)
+expsmooth_lrnr <- make_learner(Lrnr_expSmooth)
+arima_strat_lrnr <- Lrnr_multiple_ts$new(learner = lrnr_arima)
+expsmooth_strat_lrnr <- Lrnr_multiple_ts$new(learner = lrnr_expSmooth)
 
-# do one by one for now
-lrnr_arima_strat <- Lrnr_multiple_ts$new(learner = lrnr_arima)
-lrnr_expSmooth_strat <- Lrnr_multiple_ts$new(learner = lrnr_expSmooth)
-### stack of base learners
-stack <- make_learner(
-  Stack, 
-  unlist(list(xgb_learners, lrnr_glm, lrnr_lasso, lrnr_ranger, lrnr_earth,
-              lrnr_gts, lrnr_alan, lrnr_arima_strat, lrnr_polspline,
-              lrnr_expSmooth_strat), recursive = TRUE)
-)
+# library for Stack
+stack_lib <- unlist(list(mean_lrnr, glm_lrnr, ridge_lrnr, lasso_lrnr,
+                         enet_lrnr_reg25, enet_lrnr_reg50, enet_lrnr_reg75,
+                         ranger_lrnr_base, ranger_lrnr_nt50, ranger_lrnr_nt100,
+                         ranger_lrnr_nt500, earth_lrnr, xgb_lrnrs, alan_lrnr,
+                         gts_lrnr, arima_strat_lrnr, expsmooth_strat_lrnr),
+                    recursive = TRUE)
 
-### screeners
-screener_lasso <- make_learner(Lrnr_screener_coefs, lrnr_lasso, 
-                               threshold = 1e-1)
-screener_lasso_flex <- make_learner(Lrnr_screener_coefs, lrnr_lasso, 
-                                    threshold = 1e-3)
+# make stack for SL
+stack_lrnrs <- make_learner(Stack, stack_lib)
+
+# LASSO screener
+screener_lasso <- make_learner(Lrnr_screener_coefs, lrnr_lasso,
+                               threshold = 1e-2)
+#screener_lasso <- make_learner(Lrnr_screener_coefs, lrnr_lasso,
+                               #threshold = 1e-1)
+#screener_lasso_flex <- make_learner(Lrnr_screener_coefs, lrnr_lasso,
+                                    #threshold = 1e-3)
+
 # pipelines
-screen_lasso_pipe <- make_learner(Pipeline, screener_lasso, stack)
-screen_lasso_flex_pipe <- make_learner(Pipeline, screener_lasso_flex, stack)
+#screen_lasso_pipe <- make_learner(Pipeline, screener_lasso, stack)
+#screen_lasso_flex_pipe <- make_learner(Pipeline, screener_lasso_flex, stack)
+screen_lasso_pipe <- make_learner(Pipeline, screener_lasso, stack_lrnrs)
 
-### final stack
-stack <- make_learner(Stack, screen_lasso_pipe, screen_lasso_flex_pipe)
+
+# final stack for SL
+#stack_screeners <- make_learner(Stack, screen_lasso_pipe,
+                                #screen_lasso_flex_pipe)
+sl_stack_run_forrest_run <- make_learner(Stack, screen_lasso_pipe)
 
 # simple covariate screening
 sl3_debug_mode()
-sl <- generate_learners(stack = stack)
+sl <- generate_learners(stack = sl_stack_run_forrest_run)
 data <- read.csv(here("Data", "training_processed.csv"))
 test_data <- read.csv(here("Data", "test_processed.csv"))
 
