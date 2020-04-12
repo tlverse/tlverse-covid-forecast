6194966a01ee620052f5331e19ad48e81d0513c9
Nima Hejazi <nh@nimahejazi.org>
2020-04-11 18:31:00 -0700

    check coefs for table

--- R/utils.R
+++ R/utils.R
@@ -1,32 +1,39 @@
 make_sl_table <- function(sl_lrnr_fit,
                           file_out,
                           loss_fun = sl3::loss_squared_error,
-                          format = c("latex", "markdown")) {
-  ###############################################################
-  # sl_lrnr_fit : A trained sl3 Lrnr_sl object.
-  # file_out    : The name of the file to which to write.
-  # loss_fun    : A loss function used to evaluate the CV-risk.
-  #               Default is sl3::loss_squared_error
-  # format      : A file type indicating the format of the table.
-  #               Default is LaTeX.
-  ###############################################################
+                          format_out = c("latex", "markdown")) {
+  #####################################################################
+  # sl_lrnr_fit  :   A trained Super Learner (sl3::Lrnr_sl) object.
+  # file_out     :   The name of the file to which to write.
+  # loss_fun     :   A loss function used in evaluating the CV-risk.
+  #                  Default is sl3::loss_squared_error.
+  # format_out   :   A file type indicating the format of the table.
+  #                  Default is LaTeX.
+  #####################################################################
 
   # catch formatting
-  format <- match.arg(format)
-  format_ext <- dplyr::case_when(format == "latex" ~ ".tex",
-                                 format == "markdown" ~ ".md")
+  format_out <- match.arg(format_out)
+  format_ext <- dplyr::case_when(format_out == "latex" ~ ".tex",
+                                 format_out == "markdown" ~ ".md")
 
   # create CV-risk output
   sl_lrnr_risks <- sl_lrnr_fit$cv_risk(loss_fun)
 
+  # check metalearner fits and collapse coefficients if necessary
+  coefs <- sapply(sl_lrnr_fit$fit_object$cv_meta_fit$fit_object, stats::coef)
+  if (dim(coefs)) {
+    coefs_collapsed <- as.numeric(apply(coefs, 1, mean))
+    sl_lrnr_risks[, 2] <- coefs_collapsed
+  }
+
   # open file for writing table
   sink(here::here("tables", paste0(file_out, format_ext)))
   sl_lrnr_risks[, c(1, 2, 6, 3, 7)] %>%
-    knitr::kable(format = format,
+    knitr::kable(format = format_out,
                  booktabs = TRUE,
                  label = "sl_coefs_risks",
                  col.names = c("Learner", "Weight", "Min. Fold Risk",
-                               "Mean CV-Risk", "Max. Fold Risk"),
+                               "CV-Mean Risk", "Max. Fold Risk"),
                  caption = "Estimated CV-risk of learning algorithms") %>%
     kableExtra::kable_styling() %>%
     print()
